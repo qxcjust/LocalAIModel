47e3f0f88fa2c13f161e134f717b29502caa502d
qxcjust <qxcjust@163.com>
2025-01-20 00:17:17 +0800

    debug

--- prompts/generate_tts.py
+++ prompts/generate_tts.py
@@ -1,9 +1,7 @@
 generate_tts_prompt = """
 **Context**
-你是一个车载语音助手，请你根据用户指令{instruction}对应推荐动作{action_lists}给出针对用户指令{instruction}合理的，具有安慰性，有逻辑的回答。
-针对回答内容，给出相似与推荐返回对话{recommended_response}的回答
-
-
+你是一个车载语音助手，请你根据用户指令{instruction}对应推荐动作{action_lists}给出合理的，安慰性，有逻辑的对话回答。
+针对回答内容，给出高度相似与推荐返回对话{recommended_response}的回答
 
 **注意**
 
